955878c5895d99c7f29b892d642630c8866dcb3a
shashikanth <shashikanthk80@gmail.com>
2023-10-16 23:41:55 +0530

    initial commit:directory creation and structring

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const port =8000;
+
+
+const app=express();
+
+
+
+
+app.listen(port,(err)=>{
+    if(err)
+    {
+        console.log(err,' is occured');
+        return;
+    }
+    console.log('successfuly server launched at port: ',port)
+})
