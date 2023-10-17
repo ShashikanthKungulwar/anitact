d47b84ba3b878cbc67d979c7e26e9c11816befd8
shashikanth <shashikanthk80@gmail.com>
2023-10-17 18:29:08 +0530

    set view engine

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port =8000;
 
 const app=express();
 app.use('/',require('./routes/index'));
-
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,(err)=>{
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const profileController=require('../contollers/profileController');
-
+const postsController=require('../contollers/postsController');
 router.get('/profile',profileController.profile);
-
+router.get('/posts',postsController.posts);
 
 module.exports=router;
