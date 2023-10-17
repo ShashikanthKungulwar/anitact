c2eac494a64c1720e371aa0094ba55a2bc49b2a5
shashikanth <shashikanthk80@gmail.com>
2023-10-17 17:56:16 +0530

    add router and controller

---
+++ contollers/homeController.js
@@ -0,0 +1,3 @@
+module.exports.home=(req,res)=>{
+    return res.end('<h1>checking for the home page route</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express=require('express');
-
+const homerController=require('../contollers/homeController');
 const router=express.Router();
 
+
+
+router.get('/',homerController.home);
 console.log("running")
 module.exports=router;
