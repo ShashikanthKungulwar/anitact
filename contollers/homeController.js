d25cdeeaf29dd9edac7c69713253ad18d8b76f8b
shashikanth <shashikanthk80@gmail.com>
2023-10-22 16:37:12 +0530

    provide basic manual auth

--- contollers/homeController.js
+++ contollers/homeController.js
@@ -1,6 +1,5 @@
 module.exports.home=(req,res)=>{
     console.log(req.cookies);
-    res.cookie("use","shashi");
     return res.render("../views/home.ejs",{
         title:"home"
     });
--- contollers/usersController.js
+++ contollers/usersController.js
@@ -1,42 +1,63 @@
-const Users=require('../models/user');
+const Users = require('../models/user');
 
-module.exports.users=(req,res)=>{
-    return res.render("../views/users.ejs",{
-        title:"users"
+module.exports.users = (req, res) => {
+    return res.render("../views/users.ejs", {
+        title: "users"
     });
 }
 
-module.exports.signUp=(req,res)=>{
-    return res.render("../views/sign_up.ejs",{
-        title:"sign-up"
+module.exports.signUp = (req, res) => {
+    return res.render("../views/sign_up.ejs", {
+        title: "sign-up"
     });
 }
-module.exports.signIn=(req,res)=>{
-    return res.render("../views/sign_in.ejs",{
-        title:"sign-in"
+module.exports.signIn = (req, res) => {
+    return res.render("../views/sign_in.ejs", {
+        title: "sign-in"
     });
 }
 
 
-module.exports.create=async (req,res)=>{
-    if(req.body.password != req.body.confirm_password)
-    {
+module.exports.create = async (req, res) => {
+    if (req.body.password != req.body.confirm_password) {
         return res.redirect('back');
     }
-    Users.findOne({email:req.body.email}).exec().then(async result=>{
-        if(!result)
-    {
-        await Users.create(req.body);
-        return res.redirect('sign-in')
-    }
-    }).catch(err=>{
+    Users.findOne({ email: req.body.email }).exec().then(async result => {
+        if (!result) {
+            await Users.create(req.body);
+            return res.redirect('sign-in')
+        }
+    }).catch(err => {
         return res.redirect('back')
     });
-    
+
 
 }
 
 
-module.exports.createSession=(req,res)=>{
-    
+module.exports.createSession = async (req, res) => {
+
+    //find user
+
+    //if user exists then match the password
+
+    //if pattern matches then login 
+
+
+    //if not then return 
+
+    //else return back to the same page
+    let k = await Users.findOne({ email: req.body.email }).exec();
+    console.log(k);
+    if (k) {
+        if (k.password == req.body.password) {
+            console.log('success');
+            res.cookie("user_id", k.id);
+            return res.redirect('../../');
+        }
+        console.log('password didnt match');
+        return res.redirect('back');
+    }
+    console.log('user doesnt exist')
+    return res.redirect('back');
 }
