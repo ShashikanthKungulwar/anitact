3ff6c9ee5b4fb63a4333430a8d3f714d6854beed
shashikanth <shashikanthk80@gmail.com>
2023-10-28 23:41:29 +0530

    display posts in home

--- contollers/homeController.js
+++ contollers/homeController.js
@@ -1,5 +1,17 @@
-module.exports.home=(req,res)=>{
-    return res.render("../views/home.ejs",{
-        title:"home"
-    });
+const Posts = require("../models/post");
+
+module.exports.home = async (req, res) => {
+
+    try {
+        var posts = await Posts.find({}).populate('user').exec();
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+        return res.render("../views/home.ejs", {
+            title: "home",
+            posts
+        });
+    }
 }
